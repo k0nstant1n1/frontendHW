8b077ee044eb60990cf4ced99cadf4ecc8c6f968
konstantin <ziuzinkonstantin@gmail.com>
2017-10-10 20:47:31 +0300

    add 4 div and arrows-manage

--- DOM/div/div.js
+++ DOM/div/div.js
@@ -1,33 +1,56 @@
 
 window.addEventListener('load', onloadfunk);
 function onloadfunk(){
-    var div = document.querySelector('div');
-    div.style.left='200px';
-    div.style.top='200px';
+    var divBlue = document.getElementsByTagName('div')[0];
+    divBlue.style.backgroundColor='blue';
+    divBlue.style.left='200px';
+    divBlue.style.top='200px';
+    divBlue.addEventListener('click', setBehaviorToDiv);
 
-    function moveDown(px){
-        var top = +div.style.top.slice(0,div.style.top.length - 2);
-        div.style.top = top + px + 'px';
-    }
-    function moveTop(px){
-        var top = +div.style.top.slice(0,div.style.top.length - 2);
-        div.style.top = top - px + 'px';
-    }
-    function moveLeft(px){
-        var left = +div.style.left.slice(0, div.style.left.length - 2);
-        div.style.left = left - px + 'px';
-    }
-    function moveRight(px){
-        var left = +div.style.left.slice(0, div.style.left.length - 2);
-        div.style.left = left + px + 'px';
-    }
-    function movediv(event){
-        switch(event.charCode){
-            case 115: moveDown(10); break;
-            case 97 : moveLeft(10); break;
-            case 119: moveTop(10); break;
-            case 100: moveRight(10); break;
+    var divGreen = document.getElementsByTagName('div')[1];
+    divGreen.style.backgroundColor='green';
+    divGreen.style.left='200px';
+    divGreen.style.top='200px';
+    divGreen.addEventListener('click', setBehaviorToDiv);
+
+    var divRed = document.getElementsByTagName('div')[2];
+    divRed.style.backgroundColor='red';
+    divRed.style.left='200px';
+    divRed.style.top='200px';
+    divRed.addEventListener('click', setBehaviorToDiv);
+
+    var divYellow = document.getElementsByTagName('div')[3];
+    divYellow.style.backgroundColor='yellow';
+    divYellow.style.left='200px';
+    divYellow.style.top='200px';
+    divYellow.addEventListener('click', setBehaviorToDiv);
+
+    function setBehaviorToDiv(){
+        var div = this;
+        div.addEventListener('keydown',moveDiv);
+        function moveDown(px){
+            var top = +div.style.top.slice(0, div.style.top.length - 2);
+            div.style.top = top + px + 'px';
+        }
+        function moveTop(px){
+            var top = +div.style.top.slice(0, div.style.top.length - 2);
+            div.style.top = top - px + 'px';
+        }
+        function moveLeft(px){
+            var left = +div.style.left.slice(0, div.style.left.length - 2);
+            div.style.left = left - px + 'px';
+        }
+        function moveRight(px){
+            var left = +div.style.left.slice(0, div.style.left.length - 2);
+            div.style.left = left + px + 'px';
+        }
+        function moveDiv(event){
+            switch(event.keyCode){
+                case 37 : moveLeft(10); break;
+                case 38 : moveTop(10); break;
+                case 39 : moveRight(10); break;
+                case 40 : moveDown(10); break;
+            }
         }
     }
-    div.addEventListener('keypress', movediv);
 }
